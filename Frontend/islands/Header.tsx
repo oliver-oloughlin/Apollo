fa18710638540332e7a2d5830e4e57ed951e1bfc
= <=>
2022-09-02 00:19:46 +0200

    Further refined Header and mobile styling

--- Frontend/islands/Header.tsx
+++ Frontend/islands/Header.tsx
@@ -1,15 +1,33 @@
 /** @jsx h */
-import { h } from "preact";
+import { h, Fragment } from "preact";
+import { useState } from "preact/hooks"
 
 export default function Header() {
+  const [open, setOpen] = useState<boolean>(false);
+
   return (
     <header>
       <div class="header-content-horisontal">
-        <p class="header-item">ITEM</p>
+        <Items />
       </div>
       <div class="header-content-vertical">
-        <p class="header-item">ITEM</p>
+        <div onClick={() => setOpen(o => !o)} class="header-dropdown-btn icon" />
+        {open &&
+          <div class="header-dropdown-container">
+            <Items />
+          </div>
+        }
       </div>
     </header>
+  )
+}
+
+function Items() {
+  return (
+    <Fragment>
+      <p class="header-item">POLLS</p>
+      <p class="header-item">EXAMPLE</p>
+      <p class="header-item">OTHER ITEM</p>
+    </Fragment>
   )
 }
