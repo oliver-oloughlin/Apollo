04dd50c1482ce81c70871ed88ee2062e88e9761d
h584974 <oliveroloughlin@hotmail.com>
2022-09-01 14:58:41 +0200

    Refinement to switch buttons

--- Frontend/routes/index.tsx
+++ Frontend/routes/index.tsx
@@ -12,8 +12,8 @@ export default function Home() {
       </Head>
       <main class="page box-bg">
         <div class="btn-switch">
-          <button class="btn-red btn-round btn-big">NO</button>
-          <button class="btn-blue btn-round btn-big">YES</button>
+          <button class="btn-red btn-big" />
+          <button class="btn-blue btn-big" />
         </div>
       </main>
     </App>
