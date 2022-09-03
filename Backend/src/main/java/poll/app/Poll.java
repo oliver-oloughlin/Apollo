d8336eb8506baf7e40b9fd63856df1cc725d12c5
Inge <iha001@uib.no>
2022-09-03 18:16:32 +0200

    made pollCode generator

--- Backend/src/main/java/poll/app/Poll.java
+++ Backend/src/main/java/poll/app/Poll.java
@@ -1,18 +1,21 @@
 package poll.app;
 
+import java.util.Random;
+
 public class Poll {
 	
 	int ID; // Poll ID to show on screen for connection
 	String AUTHOR; // Author of Poll
-	int POLLCODE;
+	String POLLCODE;
 	
 	public Poll(String author) {
 		this.AUTHOR = author;
 		this.POLLCODE = makePollCode();
 	}
 
-	private int makePollCode() {
-		// TODO make algorithm for Code
+	private String makePollCode() {
+		Random random = new Random;
+		
 		return 0;
 	}
 	
