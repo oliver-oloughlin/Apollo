db74540a30b6284a39ed136d0c968b823772eb75
Inge <iha001@uib.no>
2022-09-03 17:53:37 +0200

    Added a poll to backend, added a private class for Questions

--- Backend/src/main/java/poll/app/Poll.java
+++ Backend/src/main/java/poll/app/Poll.java
@@ -4,9 +4,22 @@ public class Poll {
 	
 	int ID; // Poll ID to show on screen for connection
 	String AUTHOR; // Author of Poll
+	int POLLCODE;
 	
 	public Poll(String author) {
 		this.AUTHOR = author;
+		this.POLLCODE = makePollCode();
 	}
 
+	private int makePollCode() {
+		// TODO make algorithm for Code
+		return 0;
+	}
+	
+	private class Question {
+		
+		public Question() {
+			//TODO make constructor
+		}
+	}
 }
