1d1d7e29fe51ddc648248117f6f66ca6fd1cc5c6
Inge <iha001@uib.no>
2022-09-03 12:42:59 +0200

    Started making poll class with tests

---
+++ Backend/src/main/java/poll/app/Poll.java
@@ -0,0 +1,12 @@
+package poll.app;
+
+public class Poll {
+	
+	int ID; // Poll ID to show on screen for connection
+	String AUTHOR; // Author of Poll
+	
+	public Poll(String author) {
+		this.AUTHOR = author;
+	}
+
+}
