0431e421d30361750d407693beb6770e2f6169fe
Inge <iha001@uib.no>
2022-09-03 18:55:25 +0200

    Added second constructor to poll, added getters and setters to question

--- Backend/src/main/java/poll/app/Poll.java
+++ Backend/src/main/java/poll/app/Poll.java
@@ -4,10 +4,18 @@ import java.util.Random;
 
 public class Poll {
 	
-	int ID; // Poll ID to show on screen for connection
-	String AUTHOR; // Author of Poll
+	// Poll ID to show on screen for connection
+	int ID; 
+	// Author of the Poll
+	String AUTHOR; 
 	String POLLCODE;
 	Question question;
+	
+	public Poll() {
+		this.AUTHOR = "Incognito";
+		this.POLLCODE = makePollCode();
+		this.question = new Question();
+	}
 	
 	public Poll(String author) {
 		this.AUTHOR = author;
@@ -22,9 +30,34 @@ public class Poll {
 	}
 	
 	private class Question {
+		//Questions are answered by RED or GREEN votes.
+		private int red = 0;
+		private int green = 0;
 		
 		public Question() {
 			//TODO make constructor
+		}
+		
+		public void vote(boolean vote) {
+			if(vote) setRed(getRed() + 1);
+			else setGreen(getGreen() + 1);
+		}
+		
+		//Oh the boilerplate.
+		public int getGreen() {
+			return green;
+		}
+
+		public void setGreen(int green) {
+			this.green = green;
+		}
+
+		public int getRed() {
+			return red;
+		}
+
+		public void setRed(int red) {
+			this.red = red;
 		}
 	}
 }
