d171805d4325eabaec6c00acf5eb9887237fc2be
Inge <iha001@uib.no>
2022-09-03 18:23:28 +0200

    added vote to question

--- Backend/src/main/java/poll/app/Poll.java
+++ Backend/src/main/java/poll/app/Poll.java
@@ -7,16 +7,18 @@ public class Poll {
 	int ID; // Poll ID to show on screen for connection
 	String AUTHOR; // Author of Poll
 	String POLLCODE;
+	Question question;
 	
 	public Poll(String author) {
 		this.AUTHOR = author;
 		this.POLLCODE = makePollCode();
+		this.question = new Question();
 	}
 
 	private String makePollCode() {
-		Random random = new Random;
-		
-		return 0;
+		Random random = new Random();
+		int number = random.nextInt(10000);
+		return String.format("%04d", number);
 	}
 	
 	private class Question {
