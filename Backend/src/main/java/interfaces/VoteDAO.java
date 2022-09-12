4332d13c729426febb7198150a11c614c1fd2d84
584975 <584975@stud.hvl.no>
2022-09-12 11:28:18 +0200

    Added comments to VoteDAO

--- Backend/src/main/java/interfaces/VoteDAO.java
+++ Backend/src/main/java/interfaces/VoteDAO.java
@@ -4,6 +4,11 @@ import database.Vote;
 
 public interface VoteDAO {
 
+	/**
+	 * Saves a vote in the database
+	 * @param vote: The vote to be saved
+	 * @return True if the vote was saved successfully and false otherwise
+	 */
 	public boolean saveVote(Vote vote);
 	
 }
