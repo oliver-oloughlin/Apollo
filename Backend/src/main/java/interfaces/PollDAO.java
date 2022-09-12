af2cbd0639df3aaa7f32db24bf29007c997d2fdf
= <=>
2022-09-12 13:31:03 +0200

    PollDAO implementations and added utils package

--- Backend/src/main/java/interfaces/PollDAO.java
+++ Backend/src/main/java/interfaces/PollDAO.java
@@ -1,5 +1,20 @@
 package interfaces;
 
+import database.Poll;
+import utils.VoteCount;
+
+import java.util.List;
+
 public interface PollDAO {
+
+    public boolean savePoll(Poll poll);
+
+    public boolean updatePoll(Poll poll);
+
+    public Poll getPollById(int id);
+
+    public List<Poll> getAllPolls();
+
+    public VoteCount countVotesByPollId(int id);
 
 }
---
+++ Backend/src/main/java/utils/VoteCount.java
@@ -0,0 +1,28 @@
+package utils;
+
+public class VoteCount {
+
+    private int greenVotes;
+    private int redVotes;
+
+    public VoteCount(int greenVotes, int redVotes) {
+        this.greenVotes = greenVotes;
+    }
+
+    public int getGreenVotes() {
+        return greenVotes;
+    }
+
+    public void setGreenVotes(int greenVotes) {
+        this.greenVotes = greenVotes;
+    }
+
+    public int getRedVotes() {
+        return redVotes;
+    }
+
+    public void setRedVotes(int redVotes) {
+        this.redVotes = redVotes;
+    }
+
+}
