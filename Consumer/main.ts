eb8a66de9229febba005920683efda0c762a82a4
oliver-oloughlin <oliveroloughlin@hotmail.com>
2022-11-02 21:12:33 +0100

    Consumer deployed -> prod

--- Consumer/main.ts
+++ Consumer/main.ts
@@ -1,38 +1,41 @@
-import AMQP from "amqplib"
-import { SurrealDB } from "deno_surreal"
+import AMQP from "https://esm.sh/amqplib@0.10.3"
+import { SurrealDB } from "https://deno.land/x/deno_surreal@v1.5.1/mod.ts"
 
 const db = new SurrealDB({
   host: "https://apollo.fly.dev",
   user: "root",
   pass: "anderserliten69",
-  namespace: "test",
-  database: "test"
+  namespace: "apollo",
+  database: "apollo"
 })
 
-async function run() {
-  const conn = await AMQP.connect("amqp://localhost")
-  const queue = "PollQueue"
+const conn = await AMQP.connect({
+  hostname: "hawk.rmq.cloudamqp.com",
+  username: "glvuxjzx",
+  vhost: "glvuxjzx",
+  password: "53ptrDVEGoWII4-bsk7da-jg0R8VKFeo",
+  port: 5672
+})
 
-  // Consumer
-  const ch1 = await conn.createChannel()
-  await ch1.assertQueue(queue, {
-    autoDelete: false,
-    durable: false,
-    exclusive: false
-  })
-  ch1.consume(queue, async (msg) => {
-    if (msg) {
-      try {
-        const poll = JSON.parse(msg.content.toString())
-        const created = await db.create("poll", poll)
-        console.log(created)
-        ch1.ack(msg)
-      }
-      catch (err) {
-        console.error(err)
-      }
+const queue = "PollQueue"
+
+const ch1 = await conn.createChannel()
+await ch1.assertQueue(queue, {
+  autoDelete: false,
+  durable: false,
+  exclusive: false
+})
+
+ch1.consume(queue, async (msg) => {
+  if (msg) {
+    try {
+      const poll = JSON.parse(msg.content.toString())
+      const created = await db.create("poll", poll)
+      console.log(created)
+      ch1.ack(msg)
     }
-  })
-}
-
-run()
+    catch (err) {
+      console.error(err)
+    }
+  }
+})
